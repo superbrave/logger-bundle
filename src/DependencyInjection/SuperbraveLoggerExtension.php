0fe1f849ff52a41cfee7dc732d3b24a0962cd1a5
Tim van den Oever <tvdo@superbrave.nl>
2020-01-15 15:24:51 +0100

    doc: add bundle docblock to php files

--- src/DependencyInjection/SuperbraveLoggerExtension.php
+++ src/DependencyInjection/SuperbraveLoggerExtension.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file is part of the Logger bundle.
+ *
+ * @category  Bundle
+ *
+ * @author    SuperBrave <info@superbrave.nl>
+ * @copyright 2020 SuperBrave <info@superbrave.nl>
+ * @license   https://github.com/superbrave/logger-bundle/blob/master/LICENSE MIT
+ *
+ * @see       https://www.superbrave.nl/
+ */
 
 namespace Superbrave\LoggerBundle\DependencyInjection;
 
